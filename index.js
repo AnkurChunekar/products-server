3dd460e2614a0c709e7895d7aac9e7245f13fa4c
ankurtgs <89077985+AnkurChunekar@users.noreply.github.com>
2022-11-17 21:54:51 +0530

    feat: remove post request

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
@@ -13,20 +13,20 @@ const database = {
     {
       id: 1,
       name: "Kaala Chasma",
-      price: 100
+      price: 100,
     },
     {
       id: 2,
       name: "Laal Chaddi",
-      price: 55
+      price: 55,
     },
     {
       id: 3,
       name: "Peeli pant",
-      price: 101
+      price: 101,
     },
-  ]
-}
+  ],
+};
 
 // app.use(myLogger);
 
@@ -36,22 +36,17 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-
 app.get("/products", (req, res) => {
   res.json(database.products);
-})
+});
 
 app.get("/products/:id", (req, res) => {
   const productId = parseInt(req.params.id);
-  const result = database.products.find(item => item.id === productId);
-  result ?
-    res.json(result) : res.status(404).json({ message: "Product not found" });
-})
-
-app.post("/products", (req, res) => {
-  console.log(req.body);
-  res.status(201).json({ echo: req.body.product });
-})
+  const result = database.products.find((item) => item.id === productId);
+  result
+    ? res.json(result)
+    : res.status(404).json({ message: "Product not found" });
+});
 
 app.listen(port, () => {
   console.log("SERVER IS RUNNING");
