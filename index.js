dec75ede77257b2c9feedc41a41d88c1bfc49496
ankurtgs <89077985+AnkurChunekar@users.noreply.github.com>
2022-11-26 18:44:55 +0530

    feat: use authVerify and add private routes

--- index.js
+++ index.js
@@ -9,10 +9,12 @@ const productsV1 = require("./routers/products.router.v1");
 const productsV2 = require("./routers/products.router.v2");
 const usersV1 = require("./routers/users.router.v1");
 const login = require("./routers/login.router");
+const cart = require("./routers/cart.router");
 
 const routeNotFound = require("./middlewares/routeNotFound.middleware");
 const errorHandler = require("./middlewares/errorHandler.middleware");
-const authenticator = require("./middlewares/authenticator.middleware");
+// const authenticator = require("./middlewares/authenticator.middleware");
+const authVerify = require("./middlewares/authVerify.middleware");
 
 const app = express();
 const port = process.env.PORT;
@@ -37,7 +39,8 @@ app.get("/", (req, res) => {
 app.use("/login", login);
 app.use("/v1/products", productsV1);
 app.use("/v2/products", productsV2);
-app.use("/v1/users", authenticator, usersV1);
+// app.use("/v1/users", authenticator, usersV1);
+app.use("/cart", authVerify, cart);
 
 // error handling routes
 app.use(routeNotFound);
