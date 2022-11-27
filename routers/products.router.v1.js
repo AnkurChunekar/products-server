384bd15b5a23e40a73b0069e3481841e5608924f
ankurtgs <89077985+AnkurChunekar@users.noreply.github.com>
2022-11-27 12:45:09 +0530

    feat: update get and post route for products

--- routers/products.router.v1.js
+++ routers/products.router.v1.js
@@ -1,15 +1,31 @@
 const express = require("express");
-const router = express.Router();
 
-const { database } = require("../constants/constants");
+const { Product } = require("../models/product.model");
+const router = express.Router();
 
 router
   .route("/")
-  .get((req, res) => {
-    res.json(database.products);
+  .get(async (req, res) => {
+    try {
+      const products = await Product.find({});
+      res.json(products);
+    } catch (error) {
+      res.status(400).json(error);
+    }
   })
-  .post((req, res) => {
-    res.send("You want to add new product han!");
+  .post(async (req, res) => {
+    const { name, price } = req.body.product;
+    const Watch = new Product({ name, price });
+
+    try {
+      if (name === undefined || price === undefined) {
+        throw { message: "Name or price is missing!" };
+      }
+      const product = await Watch.save();
+      res.json(product);
+    } catch (error) {
+      res.status(400).json(error);
+    }
   });
 
 router.route("/:id").get((req, res) => {
